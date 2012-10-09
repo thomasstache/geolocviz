2134b22f0f97156962d43c7c3c93493c3b1ce44a
Thomas Stache <dev@thomasstache.de>
2012-10-09 17:23:36 +0200

    r.js build profile now working
    - Resolved problem between hbs and backbone depending on underscore.js
    by excluding backbone from concatenation
    - activated fileExclusionRegExp and removeCombined options for a clean
    output
    - activated useSourceUrl so the built app is still debuggable in Google
    Chrome

--- js/app.build.js
+++ js/app.build.js
@@ -4,11 +4,34 @@
 	baseUrl: "js/",
 	// output dir
 	dir: "../../geolocviz-build",
-	// name of the main script
-	name: "main",
+
 	mainConfigFile: "./main.js",
 
 	//Comment out the optimize line if you want
 	//the code minified by UglifyJS
+	// optimize: "uglify",
 	optimize: "none",
+
+	//Each script in the build layer will be turned into
+	//a JavaScript string with a //@ sourceURL comment, and then wrapped in an
+	//eval call. This allows some browsers to see each evaled script as a
+	//separate script in the script debugger even though they are all combined
+	//in the same file.
+	useSourceUrl: true,
+
+	//If set to true, any files that were combined into a build layer will be
+	//removed from the output folder.
+	removeCombined: true,
+
+	// Don't copy .git folders and files into build output
+	fileExclusionRegExp: /^\./,
+
+	modules: [
+		{
+			name: "main",
+			// Leave Backbone and dependencies (i.e. underscore) out of concatenation.
+			// This resolves a conflict with the hbs plugin. */
+			exclude: ['backbone']
+		}
+	]
 })
