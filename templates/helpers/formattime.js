9c840520204301ce1a198017d4e5c08b9e416f01
Thomas Stache <register@thomasstache.de>
2014-01-13 11:17:26 +0100

    Format epoch timestamps as Date + Time

--- templates/helpers/formattime.js
+++ templates/helpers/formattime.js
@@ -3,6 +3,8 @@ define(
 
 	function(Handlebars) {
 
+		var MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
 		/**
 		 * Handlebars helper to format values as readable time stamps.
 		 * @param  {Object} context    The value passed into the template, interpreted as milliseconds
@@ -15,14 +17,24 @@ define(
 				num = typeof context === "number" ? context : parseFloat(context);
 
 			if (!isNaN(num)) {
-				if (sourceUnit == "ms")
-					num = num / 1000;
+				// assume epoch for really large time stamps
+				if (num > MILLISECONDS_PER_DAY) {
+					var d = new Date(num);
 
-				// format as seconds
-				if (Number.prototype.toLocaleString !== undefined)
-					result = num.toLocaleString() + "s";
-				else
-					result = num.toString(10) + "s";
+					result = d.toLocaleString("de-DE");
+					if (d.getMilliseconds() > 0)
+						result += "." + d.getMilliseconds();
+				}
+				else {
+					if (sourceUnit == "ms")
+						num = num / 1000;
+
+					// format as seconds
+					if (Number.prototype.toLocaleString !== undefined)
+						result = num.toLocaleString() + "s";
+					else
+						result = num.toString(10) + "s";
+				}
 			}
 			else {
 				result = num;
@@ -30,6 +42,7 @@ define(
 
 			return new Handlebars.SafeString(result);
 		}
+
 		Handlebars.registerHelper("formattime", formatTime);
 		return formatTime;
 	}
