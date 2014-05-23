b64cc19b1144b5b294e3f9d581669f18d9cfd495
Thomas Stache <register@thomasstache.de>
2014-05-23 16:24:31 +0200

    Cellref Data Update - add good files of frequently used samples (disabled for errors: CHL_Entel_RPMO, MY_Maxis_CHR_KL, US_TMO_GPEH_SF2011) - rename existing files for consistency

--- data/data-index.js
+++ data/data-index.js
@@ -1,11 +1,20 @@
 define(
 	function() {
 		var DataIndex = [
-			{ name: "cellrefs-VfGE_NRW.txt", title: "Bochum 2013" },
-			{ name: "cellrefs-VfGE_FFM2012.txt", title: "Frankfurt/Main 2012" },
-			{ name: "cellrefs-VfGE_FFM2013.txt", title: "Frankfurt/Main 2013" },
-			{ name: "cellrefs-VfUK.txt", title: "London 2010" },
-			{ name: "cellrefs-TW_UTF8.txt", title: "Taipeh 2013" },
+			{ name: "cellrefs-DE_VfGE_FFM2012.txt", title: "Frankfurt/Main 2012" },
+			{ name: "cellrefs-DE_VfGE_FFM2013.txt", title: "Frankfurt/Main 2013" },
+			{ name: "cellrefs-DE_VfGE_NRW.txt", title: "Vf Bochum 2013" },
+			{ name: "cellrefs-NL_Vf_Amsterdam.txt", title: "Vf Amsterdam 2012" },
+			{ name: "cellrefs-UK_Vf_GPEH.txt", title: "Vf London 2010" },
+			{ name: "cellrefs-TW_CHT_ICSU_Taipeh.txt", title: "CHT Taipeh UMTS 2013" },
+			{ name: "cellrefs-BR_Claro_CHR.txt", title: "Brasilia GSM 2010" },
+			// { name: "cellrefs-CHL_Entel_RPMO.txt", title: "Chile RPMO 2013" },
+			// { name: "cellrefs-MY_Maxis_CHR_KL.txt", title: "Malaysia CHR" },
+			{ name: "cellrefs-MY_Maxis_GSM.txt", title: "Malaysia GSM 2010" },
+			{ name: "cellrefs-RO_Vf_GPEH.txt", title: "RO Vf Brasov 2010" },
+			{ name: "cellrefs-US_ATT-LTE-Houston.txt", title: "ATT Houston LTE 2013" },
+			{ name: "cellrefs-US_TMO_GPEH_SF2009.txt", title: "TMO San Francisco 2009" },
+			// { name: "cellrefs-US_TMO_GPEH_SF2011.txt", title: "TMO San Francisco 2011" },
 		];
 
 		return DataIndex;
