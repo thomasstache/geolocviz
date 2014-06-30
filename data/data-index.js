8df2ed880e28c494469be984300e55c4d9415cf3
Thomas Stache <register@thomasstache.de>
2014-06-30 11:22:45 +0200

    Add US TMO Sacramento Cellrefs Add consistent country codes

--- data/data-index.js
+++ data/data-index.js
@@ -1,21 +1,22 @@
 define(
 	function() {
 		var DataIndex = [
-			{ name: "cellrefs-DE_VfGE_FFM2012.txt", title: "Frankfurt/Main 2012" },
-			{ name: "cellrefs-DE_VfGE_FFM2013.txt", title: "Frankfurt/Main 2013" },
-			{ name: "cellrefs-DE_VfGE_NRW.txt", title: "Vf Bochum 2013" },
-			{ name: "cellrefs-DE_O2_LTE.txt", title: "o2 LTE 2014" },
-			{ name: "cellrefs-NL_Vf_Amsterdam.txt", title: "Vf Amsterdam 2012" },
-			{ name: "cellrefs-UK_Vf_GPEH.txt", title: "Vf London 2010" },
-			{ name: "cellrefs-TW_CHT_ICSU_Taipeh.txt", title: "CHT Taipeh UMTS 2013" },
-			{ name: "cellrefs-BR_Claro_CHR.txt", title: "Brasilia GSM 2010" },
-			{ name: "cellrefs-CHL_Entel_RPMO.txt", title: "Chile RPMO 2013" },
-			{ name: "cellrefs-MY_Maxis_CHR_KL.txt", title: "Malaysia CHR 2011" },
-			{ name: "cellrefs-MY_Maxis_GSM.txt", title: "Malaysia GSM 2010" },
-			{ name: "cellrefs-RO_Vf_GPEH.txt", title: "RO Vf Brasov 2010" },
-			{ name: "cellrefs-US_ATT-LTE-Houston.txt", title: "ATT Houston LTE 2013" },
-			{ name: "cellrefs-US_TMO_GPEH_SF2009.txt", title: "TMO San Francisco 2009" },
-			{ name: "cellrefs-US_TMO_GPEH_SF2011.txt", title: "TMO San Francisco 2011" },
+			{ name: "cellrefs-DE_VfGE_FFM2012.txt",    title: "DE Vf Frankfurt/Main 2012" },
+			{ name: "cellrefs-DE_VfGE_FFM2013.txt",    title: "DE Vf Frankfurt/Main 2013" },
+			{ name: "cellrefs-DE_VfGE_NRW.txt",        title: "DE Vf Bochum 2013" },
+			{ name: "cellrefs-DE_O2_LTE.txt",          title: "DE o2 LTE 2014" },
+			{ name: "cellrefs-NL_Vf_Amsterdam.txt",    title: "NL Vf Amsterdam 2012" },
+			{ name: "cellrefs-UK_Vf_GPEH.txt",         title: "UK Vf London 2010" },
+			{ name: "cellrefs-RO_Vf_GPEH.txt",         title: "RO Vf Brasov 2010" },
+			{ name: "cellrefs-BR_Claro_CHR.txt",       title: "BR Brasilia GSM 2010" },
+			{ name: "cellrefs-CHL_Entel_RPMO.txt",     title: "CHL Entel RPMO 2013" },
+			{ name: "cellrefs-MY_Maxis_CHR_KL.txt",    title: "MY Maxis CHR KL 2011" },
+			{ name: "cellrefs-MY_Maxis_GSM.txt",       title: "MY Maxis GSM 2010" },
+			{ name: "cellrefs-TW_CHT_ICSU_Taipeh.txt", title: "TW CHT Taipeh UMTS 2013" },
+			{ name: "cellrefs-US_ATT-LTE-Houston.txt", title: "US ATT Houston LTE 2013" },
+			{ name: "cellrefs-US_TMO_GPEH_SF2009.txt", title: "US TMO San Francisco 2009" },
+			{ name: "cellrefs-US_TMO_GPEH_SF2011.txt", title: "US TMO San Francisco 2011" },
+			{ name: "cellrefs-US_TMO_GPEH_SC2011.txt", title: "US TMO Sacramento 2011" },
 		];
 
 		return DataIndex;
