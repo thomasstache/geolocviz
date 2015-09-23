f203be85bac21e6b735b167afb3d101f7bdcfb2b
Thomas Stache <register@thomasstache.de>
2015-09-23 12:52:32 +0200

    Fix build script, it wasn't launching the expected (up-to-date) version of r.js

--- .build/buildglv.bat
+++ .build/buildglv.bat
@@ -1,7 +1,9 @@
 @echo off
+set OPT_PATH=%~dp0
 set BASE_PATH=D:\Thomas\Documents\GitHub
 
 cd /d %BASE_PATH%\geolocviz\js
 
-node %BASE_PATH%\r.js -o app.build.js
+node %OPT_PATH%\r.js -v
+node %OPT_PATH%\r.js -o app.build.js
 pause;
